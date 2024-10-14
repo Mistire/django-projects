6edfc9ff31ae78e87b34940b079a99b61003d016
Mistire <mdwithgod@gmail.com>
2024-10-14 12:07:10 +0300

    Implement sending email functionality

--- blog-project/myblog/blog/views.py
+++ blog-project/myblog/blog/views.py
@@ -1,14 +1,17 @@
 from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+
+from blog.forms import EmailPostForm
 from .models import Post
+from django.views.generic import ListView
+from django.core.mail import send_mail
 # Create your views here.
-def post_list(request):
-  post_list = Post.published.all()
-  paginator = Paginator(post_list, 5)
-  page_number = request.GET.get('page', 1)
-  posts = paginator.get_page(page_number)
-  return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+  queryset = Post.published.all()
+  context_object_name = 'posts'
+  paginate_by = 5
+  template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
   post = get_object_or_404(
@@ -20,3 +23,43 @@ def post_detail(request, year, month, day, post):
     publish__day=day,
   )
   return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+  post = get_object_or_404(
+    Post,
+    id=post_id,
+    status=Post.Status.PUBLISHED
+  )
+  sent = False
+
+  if request.method == 'POST':
+    form = EmailPostForm(request.POST)
+    if form.is_valid():
+      cd = form.cleaned_data
+      post_url = request.build_absolute_uri(
+        post.get_absolute_url()
+      )
+      subject = (
+        f"{cd['name']} ({cd['email']})"
+        f"recommends you read {post.title}"
+      )
+      message = (
+        f"Read {post.title} at {post_url}\n\n"
+        f"{cd['name']}\'s comments: {cd['comments']}"
+      )
+      send_mail(
+        subject=subject,
+        message=message,
+        from_email=None,
+        recipient_list=[cd['to']]
+      )
+      sent = True
+  else:
+    form = EmailPostForm()
+  return render(
+    request, 'blog/post/share.html', {
+      'post': post,
+      'form': form,
+      'sent': sent
+    }
+  )
