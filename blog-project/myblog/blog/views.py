24224b2896169975a9cff6ef45914c9e7a0614b5
Mistire <mdwithgod@gmail.com>
2024-10-15 16:26:17 +0300

    Implement functionality to retrieve similar posts based on tags

--- blog-project/myblog/blog/views.py
+++ blog-project/myblog/blog/views.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from taggit.models import Tag
+from django.db.models import Count
 
 from django.views.decorators.http import require_POST
 from blog.forms import CommentForm, EmailPostForm
@@ -45,6 +46,14 @@ def post_detail(request, year, month, day, post):
   )
   comments = post.comments.filter(active=True)
   form = CommentForm()
+
+  post_tags_ids = post.tags.values_list('id', flat=True)
+  similar_posts = Post.published.filter(
+    tags__in=post_tags_ids
+  ).exclude(id=post.id)
+  similar_posts = similar_posts.annotate(
+    same_tags=Count('tags')
+  ).order_by('-same_tags', '-publish')[:4]
   return render(
     request, 
     'blog/post/detail.html', 
@@ -52,6 +61,7 @@ def post_detail(request, year, month, day, post):
       'post': post, 
       'comments': comments,
       'form': form,
+      'similar_posts': similar_posts,
     }
   )
 
