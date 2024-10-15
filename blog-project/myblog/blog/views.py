c3e8738ac0e963bab15e627caf4b0d3ed79070e8
Mistire <mdwithgod@gmail.com>
2024-10-15 13:21:57 +0300

    Implement commenting functionality for a blog post

--- blog-project/myblog/blog/views.py
+++ blog-project/myblog/blog/views.py
@@ -2,7 +2,8 @@ from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
-from blog.forms import EmailPostForm
+from django.views.decorators.http import require_POST
+from blog.forms import CommentForm, EmailPostForm
 from .models import Post
 from django.views.generic import ListView
 from django.core.mail import send_mail
@@ -22,7 +23,17 @@ def post_detail(request, year, month, day, post):
     publish__month=month,
     publish__day=day,
   )
-  return render(request, 'blog/post/detail.html', {'post': post})
+  comments = post.comments.filter(active=True)
+  form = CommentForm()
+  return render(
+    request, 
+    'blog/post/detail.html', 
+    {
+      'post': post, 
+      'comments': comments,
+      'form': form,
+    }
+  )
 
 def post_share(request, post_id):
   post = get_object_or_404(
@@ -61,5 +72,31 @@ def post_share(request, post_id):
       'post': post,
       'form': form,
       'sent': sent
+    }
+  )
+
+@require_POST
+def post_comment(request, post_id):
+  post = get_object_or_404(
+    Post,
+    id=post_id,
+    status=Post.Status.PUBLISHED
+  )
+  comment = None
+  form = CommentForm(data=request.POST)
+  if form.is_valid():
+    comment = form.save(commit=False)
+    comment.post = post
+    comment.active = True
+    comment.save()
+  else:
+    print(form.errors)
+  return render(
+    request,
+    'blog/post/comment.html',
+    {
+      'post': post,
+      'form': form,
+      'comment': comment
     }
   )
