1ae02225a16d8bd98b35e4915dffeb5845588a85
Mistire <mdwithgod@gmail.com>
2024-11-25 12:26:06 +0300

    feat: implement shopping cart functionality
    - Built a shopping cart using Django sessions
    - Configured session settings, including expiration
    - Enabled storing shopping cart data in sessions
    - Added views for cart functionality:
      - Adding items to the cart
      - Displaying the cart contents
      - Updating product quantities in the cart
    - Created a template to display the cart
    - Added a context processor to include the current cart in all templates

--- e-commerce/myshop/myshop/settings.py
+++ e-commerce/myshop/myshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cart.apps.CartConfig',
     'shop.apps.ShopConfig',
 ]
 
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart'
             ],
         },
     },
@@ -123,6 +125,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+#for images and other media files
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
